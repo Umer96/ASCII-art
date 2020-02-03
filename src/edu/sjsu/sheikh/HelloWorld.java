a5278d4cca72d9a85b6517adad4a04b7b47cc0c8
Umer96 <51139471+Umer96@users.noreply.github.com>
2020-02-03 10:41:11 -0800

    Third Commit

--- src/edu/sjsu/sheikh/HelloWorld.java
+++ src/edu/sjsu/sheikh/HelloWorld.java
@@ -1,7 +1,9 @@
+//Muhammad Umer Sheikh
+//CS 49J
 package edu.sjsu.sheikh;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello_World");
+        System.out.println("Hello_World/SJSU");
     }
 }
