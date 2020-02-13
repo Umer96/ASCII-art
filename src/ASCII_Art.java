c4bd7540d8c6aa536ecd2af2536cd15675d60538
Umer Sheikh <umer.sheikh96@gmail.com>
2020-02-12 21:17:22 -0800

    Programming Assignment 1.1

---
+++ src/ASCII_Art.java
@@ -0,0 +1,17 @@
+public class ASCII_Art {
+    public static void main(String[] args) {
+        System.out.println("                         __________");
+        System.out.println("                        ( CLICKS!!!)"); //Bats make "clicks" sound (I googled haha)
+        System.out.println("              __________(  ________)");
+        System.out.println("             /           _/");
+        System.out.println("            ( Hello! I'm)");
+        System.out.println("            ( a bat.    )");
+        System.out.println("      (\\_/) (  _________)");
+        System.out.println("      |@ @| /_/");
+        System.out.println("  /\\  | o  \\  /  \\");
+        System.out.println(" /  \\/      \\/    \\");
+        System.out.println("/ /\\  /\\    /\\  /\\ \\");
+        System.out.println("\\/  \\/  \\  /  \\/  \\/");
+        System.out.println("         \\/");
+    }
+}
